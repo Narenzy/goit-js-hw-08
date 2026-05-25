545b917d82cc2b3cb783d31817d670ccc42f516f
Taras Makhanko <mahankotaras@gmail.com>
2026-05-25 19:42:50 +0200

    changed files structure

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const element = document.querySelector('#categories');
-const items = element.querySelectorAll('.item');
-console.log(`Number of categories: ${items.length}`);
-
-items.forEach(item => {
-  console.log(`Category: ${item.querySelector('h2').textContent}`);
-  console.log(`Elements: ${item.querySelectorAll('li').length}`);
-});
